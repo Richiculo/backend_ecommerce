2713ddde3a7268f9b3a038fc8f11530cea8ba9ff
Bruno Bulacia <beueleceia@gmail.com>
2025-04-14 00:34:20 -0400

    setup de entorno de desarrollo, conexion a la base de datos

--- backend_ecommerce/settings.py
+++ backend_ecommerce/settings.py
@@ -75,13 +75,19 @@ WSGI_APPLICATION = 'backend_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
+# CONEXION A LA BASE DE DATOS
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "ecommerce_si2",
+        "USER": "ecommerce_si2_user",
+        "PASSWORD": "HRnoRnoC9zRiOqXTuMXnDLKaMLUGcHpV",
+        "HOST": "dpg-cvu7tevgi27c73afc7cg-a.oregon-postgres.render.com",
+        "PORT": "5432"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
