20944879355d0b638a6a5c4a64575c2cf63fe3f6
Richiculo <rvargasosinaga@gmail.com>
2025-04-18 00:39:37 -0400

    cruds app productos

--- productos/models.py
+++ productos/models.py
@@ -3,33 +3,10 @@ from django.db import models
 # Modelo: Proveedor
 class Proveedor(models.Model):
     nombre=models.CharField(max_length=100)
-
-
-#Modelo: Producto
-
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(max_length=200)
-
-
-    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, null=True, blank=True)
-
+    
     def __str__(self):
         return self.nombre
 
-
-# Modelo: Detalle producto
-    
-class Detalle_Producto(models.Model):
-    marca = models.CharField(max_length=100)
-    precio_venta = models.DecimalField
-    precio_compra = models.DecimalField
-
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, null=True, blank=True)
-
-    def __str__(self):
-        return self.marca
-    
 
 # Modelo: Categoria
     
@@ -41,20 +18,48 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
 
-# Modelo: Categoria_Producto
+
+#Modelo: Producto
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=200)
+    categorias = models.ManyToManyField(Categoria, through='Categoria_Producto')
+
+
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+# Modelo: Detalle producto
     
 class Detalle_Producto(models.Model):
+    producto = models.OneToOneField(Producto, on_delete=models.CASCADE, related_name='detalle',null=True, blank=True)
+    marca = models.CharField(max_length=100, default='Generico')
+    precio_venta = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    precio_compra = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+
+    def __str__(self):
+        return f"{self.producto.nombre} - {self.marca}"
+    
+
+# Modelo: Categoria_Producto
+    
+class Categoria_Producto(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return self.producto
+        return f"{self.producto.nombre} - {self.categoria.nombre}"
 
 
 # Modelo: Stock_sucursal
     
 class Stock_sucursal(models.Model):
-    stock = models.IntegerField
+    stock = models.IntegerField(default=0)
     
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE, null=True, blank=True)
     sucursal = models.ForeignKey('sucursales.Sucursal',on_delete=models.CASCADE, null=True, blank=True)
@@ -62,5 +67,5 @@ class Stock_sucursal(models.Model):
     
 
     def __str__(self):
-        return self.nombre
+        return f"{self.producto.nombre} - {self.sucursal.nombre} - {self.stock}"
  
--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Producto, Proveedor, Detalle_Producto, Categoria, Stock_sucursal
+from .serializers import ProductoSerializer, ProveedorSerializer, DetalleProductoSerializer, CategoriaSerializer, StockSucursalSerializer
 
-# Create your views here.
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+class ProveedorViewSet(viewsets.ModelViewSet):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class DetalleProductoViewSet(viewsets.ModelViewSet):
+    queryset = Detalle_Producto.objects.all()
+    serializer_class = DetalleProductoSerializer
+
+class StockSucursalViewSet(viewsets.ModelViewSet):
+    queryset = Stock_sucursal.objects.all()
+    serializer_class = StockSucursalSerializer
+
