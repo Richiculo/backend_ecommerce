79b0bc07e69bb7035b69bf7e6243960e8efad565
Richiculo <rvargasosinaga@gmail.com>
2025-04-20 21:40:05 -0400

    gestion de descuentos funcional

--- productos/views.py
+++ productos/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Producto, Proveedor, Detalle_Producto, Categoria, Stock_sucursal, Imagen_Producto
 from .serializers import ProductoSerializer, ProveedorSerializer, DetalleProductoSerializer, CategoriaSerializer, StockSucursalSerializer, ImagenProductoSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
@@ -26,3 +30,42 @@ class StockSucursalViewSet(viewsets.ModelViewSet):
 class ImagenProductoViewSet(viewsets.ModelViewSet):
     queryset = Imagen_Producto.objects.all()
     serializer_class = ImagenProductoSerializer
+
+
+class DescuentoViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Detalle_Producto.objects.all()
+        serializer = DetalleProductoSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = DetalleProductoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        descuento = self.get_object(pk)
+        serializer = DetalleProductoSerializer(descuento, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=True, methods=['POST'])
+    def activar(self, request, pk=None):
+        descuento = self.get_object(pk)
+        descuento.tiene_descuento = True
+        descuento.save()
+        return Response({"status": "descuento activado"})
+    
+    @action(detail=True, methods=['POST'])
+    def desactivar(self, request, pk=None):
+        descuento = self.get_object(pk)
+        descuento.tiene_descuento = False
+        descuento.save()
+        return Response({"status": "descuento desactivado"})
+    
+    def get_object(self, pk):
+        return Detalle_Producto.objects.get(pk=pk)
