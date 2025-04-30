fa34187b7206aeb4d8aee0adf8dea46c418165e2
Bruno Bulacia <beueleceia@gmail.com>
2025-04-29 23:21:04 -0400

    Refactor DireccionSerializer to use CharField for departamento and update rol_cliente assignment in register function

--- direcciones/serializers.py
+++ direcciones/serializers.py
@@ -1,21 +1,9 @@
 from rest_framework import serializers
 from .models import Direccion
-
-
-class DireccionSerializer(serializers.ModelSerializer):
-    # Importación diferida para evitar el ciclo
-    def get_departamento(self, obj):
-        from sucursales.serializers import DepartamentoSerializer
-        return DepartamentoSerializer(obj.departamento).data
-
-    departamento = serializers.SerializerMethodField()  # Usar SerializerMethodField para serializar el departamento
-
 from sucursales.serializers import DepartamentoSerializer
 
-
 class DireccionSerializer(serializers.ModelSerializer):
-    # Se puede incluir el serializador de sucursal si es necesario
-    departamento = DepartamentoSerializer(read_only=True)  # Serializa el departamento relacionado
+    departamento = serializers.CharField(source='departamento.nombre', read_only=True)
     class Meta: 
         model = Direccion
         fields = '__all__'  # Serializa todos los campos del modelo
